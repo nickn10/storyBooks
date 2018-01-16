c228cc54f1eb236fac82557dbd94b57573b9fc3c
Nick Nunez <nickn10@comcast.net>
2018-01-16 17:56:30 -0600

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+   res.send('Index Page.')
+})
+
+
+
+app.listen(port, () => {
+   console.log(`Server running on port ${port}`);
+});
