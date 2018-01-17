45343f71f2e6e2a43c4e28d4e703ed99cb95dc83
Nick Nunez <nickn10@comcast.net>
2018-01-17 17:37:12 -0600

    Add express-handlebars and views folder

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('./config/config');
 const path = require('path')
 const express = require('express');
+const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
@@ -16,6 +17,9 @@ const User = require('./models/User');
 const port = process.env.PORT
 // Passport Config
 require('./config/passport')(passport);
+// Handlebars Config
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 // Middleware Config
 app.use(express.static(publicPath));
 app.use(bodyParser.json());
@@ -24,17 +28,11 @@ app.use(session({secret: 'secret', resave: false, saveUninitialized: false}));
 app.use(passport.initialize());
 app.use(passport.session());
 // Load Routes
+const index = require('./routes/index');
 const auth = require('./routes/auth');
 // Use Routes
+app.use('/', index);
 app.use('/auth', auth);
-
-app.get('/', (req, res) => {
-   res.send('Index Page.');
-});
-
-app.get('/dashboard', (req, res) => {
-   res.send('Dashboard');
-});
 
 
 
