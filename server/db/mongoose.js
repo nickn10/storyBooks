62088acb1f161cbb2a82ecbde049916af316e9e3
Nick Nunez <nickn10@comcast.net>
2018-01-17 16:57:05 -0600

    Fix mongodb_uri name to match heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MONGO_URI, { useMongoClient: true })
+mongoose.connect(process.env.MONGODB_URI, { useMongoClient: true })
 .then(() => console.log('MongoDB Connected'))
 .catch(e => console.log(e));
 
