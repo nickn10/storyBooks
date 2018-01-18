59ef601646d244f755256397b953d5974109b039
Nick Nunez <nickn10@comcast.net>
2018-01-18 14:51:08 -0600

    Add Story model and post route

--- server/routes/stories.js
+++ server/routes/stories.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const validateUser = require('../middleware/validate-login');
+const Story = require('../models/Story');
 
 // Stories Index
 router.get('/', validateUser, (req, res) => {
@@ -21,5 +22,23 @@ router.get('/edit/:id', validateUser, (req, res) => {
 router.get('/:id', validateUser, (req, res) => {
    res.render('stories/show');
 });
+
+// POST New Story
+router.post('/', validateUser, async (req, res) => {
+   try {
+      const newStory = {
+         title: req.body.title,
+         status: req.body.status,
+         allowComments: Boolean(req.body.allowComments),
+         body: req.body.body,
+         user: req.user._id
+      }
+      const addStory = new Story(newStory);
+      await addStory.save();
+      res.redirect('/stories');
+   } catch (e) {
+      res.redirect('/stories/add');
+   }
+})
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -23,6 +23,7 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 // Middleware Config
 app.use(express.static(publicPath));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(session({secret: 'secret', resave: false, saveUninitialized: false}));
