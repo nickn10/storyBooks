8d489cc088018f82d4cd49bb3735ec3e61e47af5
Nick Nunez <nickn10@comcast.net>
2018-01-18 13:37:41 -0600

    Create Add Story page

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,11 @@ const router = express.Router();
 const validateUser = require('../middleware/validate-login');
 
 router.get('/', (req, res) => {
-   res.render('index/welcome');
+   if(req.user) {
+      res.redirect('/dashboard')
+   } else {
+      res.render('index/welcome');
+   }   
 });
 
 router.get('/dashboard', validateUser, (req, res) => {
