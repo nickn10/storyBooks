f0c36a1730085d5f6126721edb749cfd2ef71ee4
Nick Nunez <nickn10@comcast.net>
2018-01-18 11:00:09 -0600

    Add  stories directory in views, and routes

--- server/routes/stories.js
+++ server/routes/stories.js
@@ -2,8 +2,24 @@ const express = require('express');
 const router = express.Router();
 const validateUser = require('../middleware/validate-login');
 
+// Stories Index
 router.get('/', validateUser, (req, res) => {
    res.render('stories/index');
+});
+
+// Add Story Form
+router.get('/add', validateUser, (req, res) => {
+   res.render('stories/add');
+});
+
+// Edit Story Form
+router.get('/edit/:id', validateUser, (req, res) => {
+   res.render('stories/edit');
+});
+
+// Show Single Story
+router.get('/:id', validateUser, (req, res) => {
+   res.render('stories/show');
 });
 
 module.exports = router;
