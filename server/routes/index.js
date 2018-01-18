6541fb3a58ad6dae26d89c0c087a5a30b64d7c78
Nick Nunez <nickn10@comcast.net>
2018-01-18 12:00:29 -0600

    Add About page

--- server/routes/index.js
+++ server/routes/index.js
@@ -10,4 +10,7 @@ router.get('/dashboard', validateUser, (req, res) => {
    res.render('index/dashboard');
 });
 
+router.get('/about', (req, res) => {
+   res.render('index/about')
+})
 module.exports = router;
